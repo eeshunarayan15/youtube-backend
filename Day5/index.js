6c461f8bbbd40ba99d14d33e814263735c760990
Eeshu Narayan <eeshunarayan15@gmail.com>
2025-07-25 03:04:34 +0530

    implemented the use of josn to parse frontend json data

---
+++ Day5/index.js
@@ -0,0 +1,33 @@
+import express from express
+const app = express();
+app.use(function (req, res, next) {
+    console.log("middle ware chala")
+    next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({encoded :true}))
+app.get("/", function (req, res) {
+  console.log("ek aur middle ware chala")
+    res.send("this is a / route")
+    
+})
+app.get("/profile", function (req, res) {
+  console.log("ek aur middle ware chala profile ke liye")
+  res.send("this is a /profile route");
+});
+app.get("/about", function (req, res) {
+  console.log("ek aur middle ware chala about ke liye");
+  res.send("this is a /about route");
+});
+app.get("/photos", function (req, res, next) {
+  return next(new Error("Something is broke"))
+  console.log("ek aur middle ware chala about ke liye");
+  res.send("this is a /about route");
+});
+app.use( function (err,req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("something went wrong")
+ 
+});
+app.listen(3000);
